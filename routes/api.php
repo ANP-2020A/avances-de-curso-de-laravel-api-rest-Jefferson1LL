e4f8443a948bf033fc71d8e15e4e297d73636727
Jefferson1LL <jefferson.rllumiquinga93@gmail.com>
2020-07-02 13:10:03 -0500

    #15 Auth Jwt Protección de rutas con el middleware

--- routes/api.php
+++ routes/api.php
@@ -18,12 +18,17 @@ use Illuminate\Http\Request;
     return $request->user();
 });*/
 
+Route::post('register', 'UserController@register');
+Route::post('login', 'UserController@authenticate');
 Route::get('articles', 'ArticleController@index');
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::get('user', 'UserController@getAuthenticatedUser');
+    Route::get('articles/{article}', 'ArticleController@show');
+    Route::post('articles', 'ArticleController@store');
+    Route::put('articles/{article}', 'ArticleController@update');
+    Route::delete('articles/{article}', 'ArticleController@delete');
+});
 
-Route::get('articles/{article}', 'ArticleController@show');
 
-Route::post('articles', 'ArticleController@store');
 
-Route::put('articles/{article}', 'ArticleController@update');
 
-Route::delete('articles/{article}', 'ArticleController@delete');
