5f6bd2daee6c055b075e85d4ebe78c7f4462d6a8
Jefferson1LL <jefferson.rllumiquinga93@gmail.com>
2020-07-16 10:12:47 -0500

    #25 validación de datos CommentController Validación de categoria de articulo Validación de titulo unico con excepcion en update

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -11,10 +11,6 @@ use Illuminate\Support\Facades\Validator;
 class ArticleController extends Controller
 {
 
-    private static $rules = [
-            'title' => 'required|string|unique:articles|max:255',
-            'body' => 'required',
-            ];
     private static $messages = [
         'required' => 'El campo :attribute es obligatorio.',
         'body.required' => 'body no es valido',
@@ -30,23 +26,30 @@ class ArticleController extends Controller
 
     public function store(Request $request){
 
-        $request -> validate(self::$rules, self::$messages);
+
+        $request->validate([
+            'title' => 'required|string|unique:articles|max:255',
+            'body' => 'required',
+            'category_id' => 'required|exists:categories, id'
+        ], self::$messages);
 
 
-
-
-        //$validator = Validator::make($request->all(), [
-        //    'title' => 'required|string|unique:articles|max:255',
-          //  'body' => 'required|string' ]);
-       // if ($validator->fails()) {
-         //   return response()->json(['error' => 'data_validation_failed',
-           //     "error_list"=>$validator->errors()], 400);
-        //}
         $article = Article::create($request->all());
         return response()->json($article, 201);
 }
 
+    /**
+     * @param Request $request
+     * @param Article $article
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function update(Request $request, Article $article){
+
+        $request->validate([
+            'title' => 'required|string|unique:articles,title,'.$article->id.'|max:255',
+            'body' => 'required',
+            'category_id' => 'required|exists:categories,id'
+        ], self::$messages);
         $article->update($request->all());
 
         return response()->json($article, 200);
